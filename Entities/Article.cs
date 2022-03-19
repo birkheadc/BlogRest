88ab48b9662d5024176732a0fa1830b7aaba28d7
Colby Birkhead <birkheadc@gmail.com>
2022-03-20 00:34:31 +0900

    Craete Article entity

--- Entities/Article.cs
+++ Entities/Article.cs
@@ -1,6 +1,11 @@
 namespace BlogRest.Entities;
 
-public class Article
+public record Article
 {
-    
+    public Guid Id { get; set; }
+    public DateTimeOffset PostDate { get; set; }
+    public string Title { get; set; }
+    public string SubTitle { get; set; }
+    public string Body { get; set; }
+
 }
