eb5151952b6726e4b4caf722b13b058e705ee87d
Colby Birkhead <birkheadc@gmail.com>
2022-03-21 20:41:17 +0900

    set up dockerfile and docker-compose

--- Contexts/TestArticleContext.cs
+++ Contexts/TestArticleContext.cs
@@ -7,15 +7,58 @@ namespace BlogRest.Contexts;
 public class TestArticleContext : IArticleContext
 {
     private string connectionString { get; set; }
+    
+    private const string databaseName = "blog";
+    private const string tableName = "articles";
 
     public TestArticleContext(IConfiguration configuration)
     {
         this.connectionString = configuration.GetConnectionString("DefaultConnection");
+        InitializeDatabase();
     }
 
     private MySqlConnection GetConnection()
     {
-        return new MySqlConnection(connectionString);
+        MySqlConnection connection = new MySqlConnection(connectionString);
+        return connection;
+    }
+
+    private void InitializeDatabase()
+    {
+        if (DoesTableExist() == false)
+        {
+            CreateTable();
+        }
+    }
+
+    private bool DoesTableExist()
+    {
+        using (MySqlConnection connection = GetConnection())
+        {
+            MySqlCommand command = new();
+            command.Parameters.AddWithValue("@db", databaseName);
+            command.Parameters.AddWithValue("@table", tableName);
+            string query = "SELECT COUNT(*) FROM information_schema.TABLES WHERE (TABLE_SCHEMA=@db) AND (TABLE_NAME=@table)";
+            command.CommandText = query;
+            command.Connection = connection;
+            connection.Open();
+            int n = int.Parse(command.ExecuteScalar().ToString() ?? "0");
+            return n > 0;
+        }
+    }
+
+    private void CreateTable()
+    {
+        using (MySqlConnection connection = GetConnection())
+        {
+            MySqlCommand command = new();
+            string query = "CREATE TABLE " + tableName + " (id CHAR(36) PRIMARY KEY NOT NULL, post_date TIMESTAMP NOT NULL, title VARCHAR(255) NOT NULL, subtitle VARCHAR(255) NOT NULL, body TEXT NOT NULL)";
+            command.CommandText = query;
+            command.Connection = connection;
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
     }
 
     public IEnumerable<Article> FindAll()
@@ -24,23 +67,42 @@ public class TestArticleContext : IArticleContext
 
         using (MySqlConnection connection = GetConnection())
         {
-            connection.Open();
-            MySqlCommand command = new("SELECT * FROM articles", connection);
-            using (MySqlDataReader reader = command.ExecuteReader())
+            try
             {
-                while (reader.Read())
+                connection.Open();
+                MySqlCommand command = new();
+                command.CommandText = "SELECT * FROM " + tableName;
+                command.Connection = connection;
+                using (MySqlDataReader reader = command.ExecuteReader())
                 {
-                    articles.Add(new Article()
+                    while (reader.Read())
                     {
-                        Id = Guid.Parse(reader["id"].ToString() ?? Guid.Empty.ToString()),
-                        PostDate = DateTimeOffset.Parse(reader["post_date"].ToString() ?? "0"),
-                        Title = reader["title"].ToString() ?? "Title Missing!",
-                        SubTitle = reader["subtitle"].ToString() ?? "SubTitle Missing!",
-                        Body = reader["body"].ToString() ?? "Body Missing"
-                    });
+                        articles.Add(new Article()
+                        {
+                            Id = Guid.Parse(reader["id"].ToString() ?? Guid.Empty.ToString()),
+                            PostDate = DateTimeOffset.Parse(reader["post_date"].ToString() ?? "0"),
+                            Title = reader["title"].ToString() ?? "Title Missing!",
+                            SubTitle = reader["subtitle"].ToString() ?? "SubTitle Missing!",
+                            Body = reader["body"].ToString() ?? "Body Missing"
+                        });
+                    }
                 }
+                connection.Close();
             }
-            connection.Close();
+            catch
+            {
+                Article article = new()
+                {
+                    Id = Guid.Empty,
+                    PostDate = DateTimeOffset.Now,
+                    Title = "Error",
+                    SubTitle = "One or more errors",
+                    Body = connectionString
+                };
+                articles.Add(article);
+                return articles;
+            }
+            
         }
         return articles;
     }
@@ -59,7 +121,7 @@ public class TestArticleContext : IArticleContext
             command.Parameters.AddWithValue("@subtitle", article.SubTitle);
             command.Parameters.AddWithValue("@body", article.Body);
             
-            command.CommandText = "INSERT INTO articles (id, post_date, title, subtitle, body) VALUES (@id, FROM_UNIXTIME(@post_date), @title, @subtitle, @body)";
+            command.CommandText = "INSERT INTO " + tableName + " (id, post_date, title, subtitle, body) VALUES (@id, FROM_UNIXTIME(@post_date), @title, @subtitle, @body)";
 
             command.Connection = connection;
 
